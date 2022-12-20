42ba76867bd5790245f75ae871a541d3fc58d943
MohamedJamh <jam7.mohamed@gamil.com>
2022-12-20 14:56:32 +0100

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
-const steps = document.getElementsByClassName('step');
-console.log(steps);
+const steps = [...document.getElementsByClassName('step')];
+const stepper = [...document.getElementsByClassName('navs')];
+// console.log(stepper[1].classList.add('active'));
 // const answers = document.querySelectorAll('.answer');
 // answers.forEach(element => {
 //     element.addEventListener('click',(e) =>{
@@ -8,8 +9,23 @@ console.log(steps);
 //         check.checked = check.checked ^ true;
 //     })
 // });
-function startQuiz(){
+// console.log(stepper[0].innerHTML);
+function navigation(currentPageIndex){
+    steps[currentPageIndex].classList.add('hidden');
+    steps[currentPageIndex+1].classList.remove('hidden');
 
+    stepper[currentPageIndex].classList.remove('active');
+    stepper[currentPageIndex+1].classList.add('active');
 }
-
+function startQuiz(){
+    navigation(0)
+}
+var count = 1;
+function nexQ(){
+    if(count == 3){
+        navigation(1)
+    }else{
+        count++
+    }
+}
 
