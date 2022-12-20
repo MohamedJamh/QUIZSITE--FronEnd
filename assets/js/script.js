4e121a98fffbf96d9d36103d8a11e8a774c4cd5f
MohamedJamh <jam7.mohamed@gamil.com>
2022-12-20 12:11:04 +0100

    commit

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+const steps = document.getElementsByClassName('step');
+console.log(steps);
+// const answers = document.querySelectorAll('.answer');
+// answers.forEach(element => {
+//     element.addEventListener('click',(e) =>{
+//         element.classList.toggle('selected');
+//         const check = element.querySelector('input');
+//         check.checked = check.checked ^ true;
+//     })
+// });
+function startQuiz(){
+
+}
+
+
